413e305067897f122404ec2279f86c8cd273b7d1
vofes <bielovlad@gmail.com>
2022-11-03 12:04:34 +0100

    Updates

---
+++ precache-manifest.562c973974bbba4f2d49694f89b5641f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "04a532e304ea4edb1f47",
+    "url": "/CNNCasaGrande/static/css/main.7118f6a5.chunk.css"
+  },
+  {
+    "revision": "04a532e304ea4edb1f47",
+    "url": "/CNNCasaGrande/static/js/main.b284f8a2.chunk.js"
+  },
+  {
+    "revision": "b5b02a32a1450850e597",
+    "url": "/CNNCasaGrande/static/js/runtime~main.bd10b9ed.js"
+  },
+  {
+    "revision": "c6d14d2f2f2fc71101ea",
+    "url": "/CNNCasaGrande/static/js/2.753727d0.chunk.js"
+  },
+  {
+    "revision": "4e17d60eb3c8bbf63848",
+    "url": "/CNNCasaGrande/static/js/3.fa6a646c.chunk.js"
+  },
+  {
+    "revision": "bcf2424c5b4a76c95c943633295904c2",
+    "url": "/CNNCasaGrande/static/media/navigationBackground.bcf2424c.png"
+  },
+  {
+    "revision": "0d23886dc5a2d0f1b4f863b6bef7883a",
+    "url": "/CNNCasaGrande/static/media/impact.0d23886d.woff"
+  },
+  {
+    "revision": "a3d9dda80dda28c8cd6977485262deb7",
+    "url": "/CNNCasaGrande/index.html"
+  }
+];
