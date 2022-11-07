1d5a0f65f117ee76e57806336a0e60849f7eb6dc
vofes <bielovlad@gmail.com>
2022-11-07 19:07:41 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CNNCasaGrande/precache-manifest.919c0bca55b16504694b4fea9d5c4339.js"
+  "/CNNCasaGrande/precache-manifest.fbac1ca9dd73af2e7e0e06e1d1cbbbc8.js"
 );
 
 workbox.clientsClaim();
